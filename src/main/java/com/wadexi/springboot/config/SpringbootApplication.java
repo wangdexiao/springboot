bfd5021e4e1c499f52d58371f624308b160ef159
wadexi <wadexi2207@163.com>
2019-12-20 04:21:38 +0800

    打jar，打jar包不能WEB-INF中静态资源打进jar，404问题解决

--- src/main/java/com/wadexi/springboot/config/SpringbootApplication.java
+++ src/main/java/com/wadexi/springboot/config/SpringbootApplication.java
@@ -19,7 +19,7 @@ import java.io.File;
 
 @Import(Dbconfig.class)
 @SpringBootApplication(scanBasePackages = "com.wadexi.springboot.web")
-public class SpringbootApplication extends SpringBootServletInitializer implements ApplicationContextAware {
+public class SpringbootApplication /*extends SpringBootServletInitializer*/ implements ApplicationContextAware {
 
     @Resource
     private ProductDao productDao;
